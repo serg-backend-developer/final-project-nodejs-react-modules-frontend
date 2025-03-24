9ec9bb7b6f95c9131558c52c56f53b187ae0be9f
Volodymyr Vovnenko <vovnenko.volodymyr@gmail.com>
2025-03-24 18:52:51 +0200

    pass API base URL with env variable

--- src/redux/categories/operations.js
+++ src/redux/categories/operations.js
@@ -1,19 +1,18 @@
-
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 import { toast } from "react-hot-toast";
 
 const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const fetchCategories = createAsyncThunk(
-  'categories/fetchCategories',
+  "categories/fetchCategories",
   async (ThunkAPI) => {
     try {
-      const response = await axios.get('https://project-team-04.onrender.com/api/categories');
+      const response = await axios.get(`${BASE_URL}/categories`);
       return response.data;
     } catch (error) {
       toast.error("Failed to load categories!");
       return ThunkAPI.rejectWithValue(error.message);
     }
-  }
+  },
 );
