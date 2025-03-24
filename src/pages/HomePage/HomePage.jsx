c3cb15210a3b707fdd959c7dcfe2034ab167dbeb
Anastasiia Dushka <143402218+Arnary@users.noreply.github.com>
2025-03-24 14:43:30 +0200

    Revert "Created markup for hero section. Added pictures."

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,15 @@
-import Hero from "../../components/Hero";
-import styles from "./HomePage.module.css"
+import css from "./HomePage.module.css";
 
 const HomePage = () => {
-  return (
-    <div className={styles.contentWrapper}>
-      <Hero />
-    </div>
-  );
+
+    return (
+        <div className={css["container"]}>
+            <div className={css["hero"]}>
+                <h1 className={css.title}>Improve Your Culinary Talents</h1>
+                <p className={css.text}>Amazing recipes for beginners in the world of cooking, enveloping you in the aromas and tastes of various cuisines.</p>
+            </div>
+        </div>
+    )
 };
 
 export default HomePage;
