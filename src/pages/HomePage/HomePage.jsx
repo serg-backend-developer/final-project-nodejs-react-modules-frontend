639161e867c1a0fdc8dd2cdcf4bb6e6eebe5f75d
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2025-03-25 19:38:26 +0100

    added RecipeList to HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import Hero from "../../components/Hero/Hero";
 import styles from "./HomePage.module.css"
 import Categories from "../../components/Categories/Categories";
+import RecipeList from "../../components/RecipeList/RecipeList";
 
 const HomePage = () => {
   return (
     <div className={styles.contentWrapper}>
       <Hero />
       <Categories />
+      <RecipeList />
     </div>
   );
 };
