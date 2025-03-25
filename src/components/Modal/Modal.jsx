5569b45b066acfbc4cdedec04525e446b4157193
Arnary <anastasia.dushka1@gmail.com>
2025-03-25 16:41:54 +0200

    add close modal by Esc

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,6 +4,11 @@ import styles from './Modal.module.css';
 
 const Modal = ({ isOpen, onClose, children, className = '' }) => {
   if (!isOpen) return null;
+  document.addEventListener('keydown', function(e) {
+  if (e.key === 'Escape') {
+    onClose();
+  }
+});
   return ReactDOM.createPortal(
     <div className={styles.overlay} onClick={onClose}>
       <div className={`${styles.modal} ${className}`} onClick={e => e.stopPropagation()}>
