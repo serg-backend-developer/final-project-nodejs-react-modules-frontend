c50b614068824d423f2c5bc63464d5021bbfdecb
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2025-03-26 21:16:00 +0100

    fix container

--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -7,6 +7,7 @@ import Subtitle from "../Subtitle/Subtitle.jsx";
 import RecipeList from "../RecipeList/RecipeList.jsx";
 import RecipeFilters from "../RecipeFilters/RecipeFilters.jsx";
 
+import style from '../App.module.css';
 import css from "./Recipes.module.css";
 import icons from "../../img/icons2.svg";
 
@@ -19,10 +20,10 @@ const Recipes = ({ category }) => {
 	const location = useLocation();
 
 	// const getQueryParams = (search) => {
-    //     return new URLSearchParams(search);
-    // };
+	//     return new URLSearchParams(search);
+	// };
 
-    // const queryParams = getQueryParams(location.search);
+	// const queryParams = getQueryParams(location.search);
 	// const page = queryParams.get("page") || 1;
 
 	const handleChangeFilter = (selectedId, type) => {
@@ -32,17 +33,16 @@ const Recipes = ({ category }) => {
 	};
 
 	// const handlerChangeCategory = (category) => {
-    //     setCategory(category);
+	//     setCategory(category);
 
-
-    //     if (categoriesListElement.current) {
-    //         setTimeout(() => {
-    //             if (categoriesListElement.current) {
-    //                 categoriesListElement.current.scrollIntoView({behavior: 'smooth'});
-    //             }
-    //         }, 100);
-    //     }
-    // };
+	//     if (categoriesListElement.current) {
+	//         setTimeout(() => {
+	//             if (categoriesListElement.current) {
+	//                 categoriesListElement.current.scrollIntoView({behavior: 'smooth'});
+	//             }
+	//         }, 100);
+	//     }
+	// };
 
 	const isEmptyObject = (obj) => {
 		return Object.keys(obj).length === 0 && obj.constructor === Object;
@@ -56,10 +56,12 @@ const Recipes = ({ category }) => {
 			try {
 				const url = `https://project-team-04.onrender.com/api/recipes`;
 				// const url = `/api/recipes?page=${page}&limit=6&category=${category.id}`
-                //     + (areaId ? `&area=${areaId}` : '')
-                //     + (ingredientId ? `&ingredient=${ingredientId}` : '');
+				//     + (areaId ? `&area=${areaId}` : '')
+				//     + (ingredientId ? `&ingredient=${ingredientId}` : '');
 				const response = await axios.get(url);
-				const filteredRecipes = response.data.recipes.filter(recipe => recipe.categoryId === category.id);
+				const filteredRecipes = response.data.recipes.filter(
+					(recipe) => recipe.categoryId === category.id
+				);
 				setRecipes(filteredRecipes);
 				setTotal(filteredRecipes.length);
 			} catch (error) {
@@ -73,40 +75,42 @@ const Recipes = ({ category }) => {
 
 	return (
 		<section>
-			{!isEmptyObject(category) &&
-				<button className={css.btn}>
-                    <svg className={css.icon}>
-                        <use href={`${icons}#icon-arrow-up-right`}></use>
-                    </svg>
-                    back
-                </button>
-			}
-			<div ref={categoriesListElement}></div>
-			<MainTitle>
-				{isEmptyObject(category) ? "Categories" : category.name}
-			</MainTitle>
-			<Subtitle>
-				{isEmptyObject(category)
-					? `Discover a limitless world of culinary possibilities and enjoy
+			<div className={style.container}>
+				{!isEmptyObject(category) && (
+					<button className={css.btn}>
+						<svg className={css.icon}>
+							<use href={`${icons}#icon-arrow-up-right`}></use>
+						</svg>
+						back
+					</button>
+				)}
+				<div ref={categoriesListElement}></div>
+				<MainTitle>
+					{isEmptyObject(category) ? "Categories" : category.name}
+				</MainTitle>
+				<Subtitle>
+					{isEmptyObject(category)
+						? `Discover a limitless world of culinary possibilities and enjoy
           exquisite recipes that combine taste, style and the warm atmosphere of
           the kitchen.`
-                    : `Go on a taste journey, where every sip is a sophisticated creative chord,
+						: `Go on a taste journey, where every sip is a sophisticated creative chord,
           and every dessert is an expression of the most refined gastronomic desires.`}
-			</Subtitle>
-			<div className={css["recipes-category"]}>
-				<div>
-					<RecipeFilters changeHandler={handleChangeFilter} />
-				</div>
-				<div>
-					<div className={css["recipes-list"]}>
-						<RecipeList recipes={recipes} />
-						{recipes.length === 0 && (
-							<div className={css["no-recipes"]}>
-								<p>No recipes found</p>
-							</div>
-						)}
+				</Subtitle>
+				<div className={css["recipes-category"]}>
+					<div>
+						<RecipeFilters changeHandler={handleChangeFilter} />
 					</div>
-					{/* <Pagination total={total} limit={6}/> */}
+					<div>
+						<div className={css["recipes-list"]}>
+							<RecipeList recipes={recipes} />
+							{recipes.length === 0 && (
+								<div className={css["no-recipes"]}>
+									<p>No recipes found</p>
+								</div>
+							)}
+						</div>
+						{/* <Pagination total={total} limit={6}/> */}
+					</div>
 				</div>
 			</div>
 		</section>
