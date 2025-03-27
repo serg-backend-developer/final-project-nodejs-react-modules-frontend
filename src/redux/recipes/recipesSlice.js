93fd07d06f925cdb111fd8415343beeb769242fe
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2025-03-27 16:11:15 +0100

    devided into two separated slices

--- src/redux/recipes/recipesSlice.js
+++ src/redux/recipes/recipesSlice.js
@@ -1,51 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
-import {
-	addFavoriteRecipe,
-	removeFromFavoriteRecipe,
-	getFavoriteRecipes,
-	fetchRecipes,
-} from "./operations";
-
-const initialState = {
-	selectedRecipe: "",
-	favoriteRecipes: [],
-	myFavorites: {
-		total: 0,
-		favoriteRecipes: [],
-	},
-	recipes: {
-		total: 0,
-		result: [],
-	},
-	isLoadingFavorite: false,
-	isErrorFavorite: null,
-	isLoading: false,
-	isError: null,
-	popularRecipes: [],
-	isLoadingPopular: false,
-	isErrorPopular: null,
-	recipeCreate: {
-		lastCreatedRecipe: null,
-		isLoading: false,
-		isError: null,
-	},
-	recipeDelete: {
-		isLoading: false,
-		isError: null,
-	},
-	fetchRecipes: {
-		isLoading: false,
-		isError: null,
-		result: {
-			total: 0,
-			recipes: [],
-		},
-	},
-};
+import { fetchRecipes } from "./operations";
 
 const recipeSlice = createSlice({
 	name: "recipes",
-	initialState,
+	initialState: {
+		list: [],
+		selectedRecipe: "",
+		status: "idle",
+		error: null,
+	},
 	reducers: {
 		selectRecipe: (state, action) => {
 			state.selectedRecipe = action.payload;
@@ -53,53 +16,16 @@ const recipeSlice = createSlice({
 	},
 	extraReducers: (builder) =>
 		builder
-			.addCase(getFavoriteRecipes.pending, (state, action) => {
-				state.isLoading = true;
-				state.isError = null;
-			})
-			.addCase(getFavoriteRecipes.rejected, (state, action) => {
-				state.isLoading = false;
-				state.isError = action.payload;
-			})
-			.addCase(getFavoriteRecipes.fulfilled, (state, action) => {
-				state.isLoading = false;
-				state.favoriteRecipes = action.payload;
-			})
-			.addCase(addFavoriteRecipe.pending, (state, action) => {
-				state.isLoadingFavorite = true;
-				state.isErrorFavorite = null;
-			})
-			.addCase(addFavoriteRecipe.rejected, (state, action) => {
-				state.isLoadingFavorite = false;
-				state.isErrorFavorite = action.payload;
-			})
-			.addCase(addFavoriteRecipe.fulfilled, (state, action) => {
-				state.isLoadingFavorite = false;
-				state.isErrorFavorite = null;
-			})
-			.addCase(removeFromFavoriteRecipe.pending, (state, action) => {
-				state.isLoadingFavorite = true;
-				state.isErrorFavorite = null;
-			})
-			.addCase(removeFromFavoriteRecipe.rejected, (state, action) => {
-				state.isLoadingFavorite = false;
-				state.isErrorFavorite = action.payload;
-			})
-			.addCase(removeFromFavoriteRecipe.fulfilled, (state, action) => {
-				state.isLoadingFavorite = false;
-				state.isErrorFavorite = null;
-			})
 			.addCase(fetchRecipes.pending, (state) => {
-				state.fetchRecipes.isLoading = true;
-				state.fetchRecipes.isError = null;
+				state.status = "loading";
 			})
 			.addCase(fetchRecipes.rejected, (state, action) => {
-				state.fetchRecipes.isLoading = false;
-				state.fetchRecipes.isError = action.payload;
+				state.status = "failed";
+				state.error = action.error.message;
 			})
 			.addCase(fetchRecipes.fulfilled, (state, action) => {
-				state.fetchRecipes.isLoading = false;
-				state.fetchRecipes.result = action.payload;
+				state.status = "succeeded";
+				state.list = action.payload;
 			}),
 });
 
