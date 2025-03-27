f7334a1e302ff61cae7957bc560bd013d5e6ee92
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2025-03-27 08:57:32 +0100

    fixed avatar and name

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,5 +1,7 @@
 import { NavLink } from "react-router-dom";
-
+import { useState, useEffect } from "react";
+import axios
+	from "axios";
 import icons from "../../img/icons2.svg";
 import css from "./RecipeCard.module.css";
 import noImage from "../../img/empty/no-image.png";
@@ -9,6 +11,17 @@ import { defaultUserName } from "../../constants/constants";
 import RecipeCardFavoriteButton from "../RecipeCardFavoriteButton/RecipeCardFavoriteButton";
 
 export const RecipeCard = ({ recipe, isFavorite }) => {
+	const [owner, setOwner] = useState(null);
+
+	useEffect(() => {
+		axios.get(`$https://project-team-04.onrender.com/users/${recipe.ownerId}`)
+			.then(response => {
+				console.log("response", response.data);
+				setOwner(response.data);
+			})
+            .catch(() => setOwner(null));
+	}, [recipe.ownerId]);
+
 	return (
 		<div className={css.card}>
 			<img
@@ -24,11 +37,11 @@ export const RecipeCard = ({ recipe, isFavorite }) => {
 					<div className={css.avatarContainer}>
 						<img
 							className={css.avatar}
-							src={ noAvatar}
-							alt={defaultUserName}
+							src={ owner?.avatar || noAvatar}
+							alt={owner?.name || defaultUserName}
 						/>
 					</div>
-					<h4 className={css.name}>{defaultUserName}</h4>
+					<h4 className={css.name}>{owner?.name || defaultUserName}</h4>
 				</div>
 
 				<div className={css.buttons}>
