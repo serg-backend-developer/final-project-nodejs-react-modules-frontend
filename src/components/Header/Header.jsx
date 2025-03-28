cbe4a1951199e879a7cc5d12863f6e4ad57a405d
RomanSemywolos <56417828+RomanSemywolos@users.noreply.github.com>
2025-03-28 16:27:24 +0200

    add authbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,59 @@
-import React from "react";
-import styles from "./Header.module.css";
-import { Link } from "react-router-dom";
+// src/components/Header/Header.jsx
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import AuthBar from './AuthBar/AuthBar';
+import SignInModal from '../SignInModal/SignInModal';
+import SignUpModal from '../SignUpModal/SignUpModal';
+import LogOutModal from '../LogOutModal/LogOutModal';
+import styles from './Header.module.css';
 
 const Header = () => {
+  // Стан для модальних вікон
+  const [isSignInOpen, setSignInOpen] = useState(false);
+  const [isSignUpOpen, setSignUpOpen] = useState(false);
+  const [isLogOutOpen, setLogOutOpen] = useState(false);
+
+  // Функції для перемикання модалок
+  const switchToSignIn = () => {
+    setSignUpOpen(false);
+    setSignInOpen(true);
+  };
+
+  const switchToSignUp = () => {
+    setSignInOpen(false);
+    setSignUpOpen(true);
+  };
+
   return (
-    <header>
-      <div className="container">
-        <Link to="/" className="headerLogo" aria-label="Go to homepage">
+    <header className={styles.header}>
+      <div className={styles.container}>
+        {/* Логотип */}
+        <Link to="/" className={styles.logo} aria-label="Go to homepage">
           foodies
         </Link>
-        {/* 
-          Тут будуть компоненти:
-          1) Nav (для авторизованого користувача)
-          2) AuthBar (для неавторизованого користувача) або UserBar (для авторизованого користувача)
-        */}
+        {/* AuthBar отримує callback-и для відкриття модалок */}
+        <AuthBar
+          openSignInModal={() => setSignInOpen(true)}
+          openSignUpModal={() => setSignUpOpen(true)}
+          openLogOutModal={() => setLogOutOpen(true)}
+        />
       </div>
+
+      {/* Рендеринг модальних вікон */}
+      <SignInModal
+        isOpen={isSignInOpen}
+        onClose={() => setSignInOpen(false)}
+        onSwitchToSignUp={switchToSignUp}
+      />
+      <SignUpModal
+        isOpen={isSignUpOpen}
+        onClose={() => setSignUpOpen(false)}
+        onSwitchToSignIn={switchToSignIn}
+      />
+      <LogOutModal
+        isOpen={isLogOutOpen}
+        onClose={() => setLogOutOpen(false)}
+      />
     </header>
   );
 };
