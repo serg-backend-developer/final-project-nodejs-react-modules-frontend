21f5c3f0c64899786543a7d1304171c4dac09015
RomanSemywolos <56417828+RomanSemywolos@users.noreply.github.com>
2025-03-28 17:47:30 +0200

    add userbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,54 @@
-import React from "react";
-import styles from "./Header.module.css";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import UserBar from "./UserBar/UserBar";
+import SignInModal from "../SignInModal/SignInModal";
+import SignUpModal from "../SignUpModal/SignUpModal";
+import LogOutModal from "../LogOutModal/LogOutModal";
+import styles from "./Header.module.css";
 
 const Header = () => {
+  const user = useSelector((state) => state.auth.user);
+
+  // Стан для модальних вікон
+  const [isSignInOpen, setSignInOpen] = useState(false);
+  const [isSignUpOpen, setSignUpOpen] = useState(false);
+  const [isLogOutOpen, setLogOutOpen] = useState(false);
+
+  // Функції для перемикання модалок
+  const switchToSignIn = () => {
+    setSignUpOpen(false);
+    setSignInOpen(true);
+  };
+
+  const switchToSignUp = () => {
+    setSignInOpen(false);
+    setSignUpOpen(true);
+  };
+
   return (
-    <header>
-      <div className="container">
-        <Link to="/" className="headerLogo" aria-label="Go to homepage">
+    <header className={styles.header}>
+      <div className={styles.container}>
+        {/* Логотип */}
+        <Link to="/" className={styles.logo} aria-label="Go to homepage">
           foodies
         </Link>
-        {/* 
-          Тут будуть компоненти:
-          1) Nav (для авторизованого користувача)
-          2) AuthBar (для неавторизованого користувача) або UserBar (для авторизованого користувача)
-        */}
+        {/* Відображення UserBar, якщо користувач автентифікований */}
+        {user && <UserBar openLogOutModal={() => setLogOutOpen(true)} />}
       </div>
+
+      {/* Модальні вікна */}
+      <SignInModal
+        isOpen={isSignInOpen}
+        onClose={() => setSignInOpen(false)}
+        onSwitchToSignUp={switchToSignUp}
+      />
+      <SignUpModal
+        isOpen={isSignUpOpen}
+        onClose={() => setSignUpOpen(false)}
+        onSwitchToSignIn={switchToSignIn}
+      />
+      <LogOutModal isOpen={isLogOutOpen} onClose={() => setLogOutOpen(false)} />
     </header>
   );
 };
