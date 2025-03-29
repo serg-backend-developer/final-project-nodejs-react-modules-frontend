8a96ea1f5648b290d99236929b3fa023b3698923
Yevhen Zakharevych <yzakharevych@fieldcomplete.com>
2025-03-29 19:25:03 +0100

    recipe-preview

--- src/components/RecipePreview/RecipePreview.jsx
+++ src/components/RecipePreview/RecipePreview.jsx
@@ -1,6 +1,34 @@
+import { MdOutlineArrowOutward } from "react-icons/md";
+import { SlTrash } from "react-icons/sl";
+import style from "./RecipePreview.module.css";
+
 const RecipePreview = ({ recipe }) => {
   const { id, title, description, thumb } = recipe;
-  return <div>{title}</div>;
+
+  return (
+    <div className={style.recipeCard}>
+      <div className={style.imageContainer}>
+        <img src={thumb} alt={title} className={style.image} />
+      </div>
+
+      <div className={style.content}>
+        <h3 className={style.title}>{title}</h3>
+        <p className={style.description}>{description}</p>
+      </div>
+
+      <div className={style.actions}>
+        <a
+          href={`/recipe/${id}`}
+          className={`${style.button} ${style.viewButton}`}
+        >
+          <MdOutlineArrowOutward />
+        </a>
+        <button className={style.button}>
+          <SlTrash />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default RecipePreview;
