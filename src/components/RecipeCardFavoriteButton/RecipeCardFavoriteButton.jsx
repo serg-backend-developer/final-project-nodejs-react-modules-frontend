65694d76588a0504f328b7416a79be3bf52ebd3d
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2025-03-29 13:40:29 +0100

    authentication for adding favorites

--- src/components/RecipeCardFavoriteButton/RecipeCardFavoriteButton.jsx
+++ src/components/RecipeCardFavoriteButton/RecipeCardFavoriteButton.jsx
@@ -8,19 +8,27 @@ import {
 	addFavoriteRecipe,
 	removeFromFavoriteRecipe,
 } from "../../redux/recipes/operations";
-import { selectIsLoadingFavorite} from "../../redux/recipes/selectors";
+import { selectIsLoadingFavorite } from "../../redux/recipes/selectors";
+import { getIsAuthenticated } from "../../redux/auth/selectors";
+import SignInModal from "../SignInModal/SignInModal";
+import SignUpModal from "../SignUpModal/SignUpModal";
 
 function RecipeCardFavoriteButton({ idRecipe, favorite }) {
 	const [isFavorite, setIsFavorite] = useState(favorite);
 	const dispatch = useDispatch();
-
 	const loading = useSelector(selectIsLoadingFavorite);
+	const [isSignInOpen, setSignInOpen] = useState(false);
+	const [isSignUpOpen, setSignUpOpen] = useState(false);
+	const isAuth = useSelector(getIsAuthenticated);
 
 	const handleAddToFavorite = async (id) => {
 		if (loading) return;
+		if (!isAuth) {
+			setSignInOpen(true);
+			return;
+		}
 		setIsFavorite(true);
 		dispatch(addFavoriteRecipe(id));
-
 	};
 
 	const handleRemoveFromFavorite = async (id) => {
@@ -28,13 +36,25 @@ function RecipeCardFavoriteButton({ idRecipe, favorite }) {
 		setIsFavorite(false);
 		dispatch(removeFromFavoriteRecipe(id));
 	};
+
+	const switchToSignUp = () => {
+    	setSignUpOpen(true);
+    	setSignInOpen(false);
+	};
+
+	const switchToSignIn = () => {
+    	setSignUpOpen(false);
+    	setSignInOpen(true);
+  	};
+
 	return (
 		<>
 			{isFavorite ? (
 				<button
 					className={css.btn}
 					type="button"
-					onClick={() => handleRemoveFromFavorite(idRecipe)}>
+					onClick={() => handleRemoveFromFavorite(idRecipe)}
+				>
 					<svg className={css.icon}>
 						<use href={`${icons}#icon-heart`}></use>
 					</svg>
@@ -43,12 +63,23 @@ function RecipeCardFavoriteButton({ idRecipe, favorite }) {
 				<button
 					className={css.btn}
 					type="button"
-					onClick={() => handleAddToFavorite(idRecipe)}>
+					onClick={() => handleAddToFavorite(idRecipe)}
+				>
 					<svg className={css.icon}>
 						<use href={`${icons}#icon-heart-outline`}></use>
 					</svg>
 				</button>
 			)}
+			<SignInModal
+				isOpen={isSignInOpen}
+				onClose={() => setSignInOpen(false)}
+				onSwitchToSignUp={switchToSignUp}
+			/>
+			<SignUpModal
+        		isOpen={isSignUpOpen}
+        		onClose={() => setSignUpOpen(false)}
+        		onSwitchToSignIn={switchToSignIn}
+      		/>
 		</>
 	);
 }
