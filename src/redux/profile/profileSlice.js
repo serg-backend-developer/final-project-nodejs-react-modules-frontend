a5163f766e49e1faa3b6fd989545c4b59a10f116
Yevhen Zakharevych <yzakharevych@fieldcomplete.com>
2025-03-30 18:05:53 +0200

    follow/unfollow profile

--- src/redux/profile/profileSlice.js
+++ src/redux/profile/profileSlice.js
@@ -1,5 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { fetchCurrentProfile, fetchProfile, updateAvatar } from "./operations";
+import {
+  fetchCurrentProfile,
+  fetchProfile,
+  updateAvatar,
+  fetchUserFollowing,
+  followProfile,
+  unfollowProfile,
+} from "./operations";
 
 const profileSlice = createSlice({
   name: "profile",
@@ -8,6 +15,7 @@ const profileSlice = createSlice({
     selectedProfile: null,
     status: "idle",
     error: null,
+    following: [],
   },
   reducers: {},
   extraReducers: (builder) => {
@@ -49,8 +57,48 @@ const profileSlice = createSlice({
       .addCase(updateAvatar.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
+      })
+      .addCase(fetchUserFollowing.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchUserFollowing.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.following = action.payload.following;
+      })
+      .addCase(fetchUserFollowing.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(followProfile.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(followProfile.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        const newFollowing = action.payload;
+        state.following.push(newFollowing);
+      })
+      .addCase(followProfile.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(unfollowProfile.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(unfollowProfile.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        const unfollowedId = action.payload.id;
+        state.following = state.following.filter(
+          (profile) => profile.id !== unfollowedId
+        );
+      })
+      .addCase(unfollowProfile.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
       });
   },
 });
+
+export const isFollowingProfile = (profileId) => (state) =>
+  state.profile.following.some((profile) => profile.id === +profileId);
 
 export default profileSlice.reducer;
