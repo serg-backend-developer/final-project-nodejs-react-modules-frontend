331ab77375b6bd1ed6ff0226e29d27028ec50fe2
Arnary <anastasia.dushka1@gmail.com>
2025-03-30 22:51:21 +0300

    update

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
+import css from "./NotFoundPage.module.css"
 
 
 const NotFoundPage = () => {
     return (
-        <Link to='/'>Page not found, please return to the home page.</Link>
+        <div className={css.notFound}>
+            <Link to='/'>Page not found, please return to the home page.</Link>
+        </div>
+        
     )
 }
 
